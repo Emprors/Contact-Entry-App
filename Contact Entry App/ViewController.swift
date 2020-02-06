638a6f50e864c841bf9565c5c03493f04d5a401c
Anour <emprors2@gmail.com>
2020-02-06 08:55:47 -0500

    Initial Commit

---
+++ Contact Entry App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contact Entry App
+//
+//  Created by Bouna Anour NDIAYE on 2020-02-06.
+//  Copyright © 2020 Bouna Anour NDIAYE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
